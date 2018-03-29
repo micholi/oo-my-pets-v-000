5a23ef3b044e2d9378516571ef66ec333956c12d
Michelle Olivieri <michelleo72@gmail.com>
2018-03-29 15:37:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -9,12 +9,13 @@ attr_reader :species
 
 # Class Methods
 
-  # can initialize an owner ??
+
+  def initialize(species, name = nil)
+  # can initialize an owner
   # initializes with a species
   # can't change its species
   # can have a name
   # is initialized with a pets attribute as a hash with 3 keys
-  def initialize(species, name = nil)
     @species = species
     @name = name
     @@all << self
